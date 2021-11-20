ae2048abfdac5846215c2c32756e1431396f6a85
Will Jhun <36206676+wjhun@users.noreply.github.com>
2021-11-20 01:14:18 -0500

    make additional closures contextual where necessary; implement context acquire and release / locking to guarantee safe frame handoff on immediate completion of contextual closure when running with multiple processors; make kernel-specific __stack_chk_fail; do syscall frame return via separate callback instead of resume; use async apply for flush:service_list(); have syscall_finish optionally reschedule thread to avoid race between schedule_thread() and syscall_finish()

--- src/unix/unix.c
+++ src/unix/unix.c
@@ -171,11 +171,12 @@ define_closure_function(0, 1, context, unix_fault_handler,
             goto bug;
         }
     } else if (is_page_fault(ctx->frame)) {
-        pf_debug("page fault, vaddr 0x%lx\n", vaddr);
+        pf_debug("page fault, vaddr 0x%lx, ctx %p, type %d, pc 0x%lx",
+                 vaddr, ctx, ctx->type, ctx->frame[SYSCALL_FRAME_PC]);
         vmap vm = vmap_from_vaddr(t->p, vaddr);
         if (vm == INVALID_ADDRESS) {
             if (user) {
-                pf_debug("no vmap found for addr 0x%lx, rip 0x%lx", vaddr, ctx->frame[SYSCALL_FRAME_PC]);
+                pf_debug("no vmap found");
                 deliver_fault_signal(SIGSEGV, t, vaddr, SEGV_MAPERR);
 
                 /* schedule this thread to either run signal handler or terminate */
